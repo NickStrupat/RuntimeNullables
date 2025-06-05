13c9ef7ab00f6ddadbbda6a2796a2bf33048b337
Nick Strupat <nick.strupat@gmail.com>
2025-06-05 14:32:55 -0400

    Remove commented methods

--- Source/RuntimeNullables.Fody/ILHelpers.cs
+++ Source/RuntimeNullables.Fody/ILHelpers.cs
@@ -40,41 +40,4 @@ internal static class ILHelpers
         instructions.Insert(index++, Instruction.Create(OpCodes.Ldstr, paramValue));
         instructions.Insert(index++, Instruction.Create(OpCodes.Call, throwHelperMethod));
     }
-
-    // /// <summary>
-    // /// Converts the value on the stack to a value ref which can be used to test for null.
-    // /// </summary>
-    // public static void InsertGetValueRef(ref int index, Collection<Instruction> instructions, TypeReference valueType)
-    // {
-    //     var elementType = valueType.GetElementType();
-    //
-    //     if (valueType.IsByReference) {
-    //         if (elementType.IsGenericParameter) {
-    //             instructions.Insert(index++, Instruction.Create(OpCodes.Ldobj, elementType));
-    //             instructions.Insert(index++, Instruction.Create(OpCodes.Box, elementType));
-    //         }
-    //         else {
-    //             instructions.Insert(index++, Instruction.Create(OpCodes.Ldind_Ref));
-    //         }
-    //     }
-    //     else if (elementType.IsGenericParameter) {
-    //         instructions.Insert(index++, Instruction.Create(OpCodes.Box, valueType));
-    //     }
-    // }
-    //
-    // public static void InsertThrowHelperCallIfValueRefIsNull(
-    //     ref int index,
-    //     Collection<Instruction> instructions,
-    //     MethodReference throwHelperMethod,
-    //     string paramValue,
-    //     Instruction continuationPoint,
-    //     ReturnBlockInfo? returnBlockInfo = null)
-    // {
-    //     var branchIfNotNull = Instruction.Create(OpCodes.Brtrue_S, continuationPoint);
-    //     returnBlockInfo?.Branches.Add(branchIfNotNull);
-    //     instructions.Insert(index++, branchIfNotNull);
-    //
-    //     instructions.Insert(index++, Instruction.Create(OpCodes.Ldstr, paramValue));
-    //     instructions.Insert(index++, Instruction.Create(OpCodes.Call, throwHelperMethod));
-    // }
 }
